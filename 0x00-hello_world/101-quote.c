19ace3c7a043cf540e6168280583e80d49965c87
gkwoode <winsleywoode@gmail.com>
2022-09-08 11:15:09 -0700

    Fix: Fixed Indentation and description

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main - entry point
+ * Description: Print same thing
+ * Return: 1
+ */
 int main(void)
 {
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
+	write(1, ch, sizeof(ch));
 	return (1);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
-#inlude <stdio.h>
-
+#include <stdio.h>
+/**
+ * main -mani block
+ * Return: 0
+ */
 int main(void)
-	{
+{
 	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return(0);
-	}
+	return (0);
+}
