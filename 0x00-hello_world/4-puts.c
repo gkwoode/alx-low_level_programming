0599d23808a5fda4461801a50f12863140389c9e
gkwoode <winsleywoode@gmail.com>
2022-09-08 11:02:09 -0700

    Fix: Fixed indentation and description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *This is the main block
- *Using puts
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
